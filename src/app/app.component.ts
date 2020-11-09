e62a3f149f123839c6d3661b3b84227468e4a3d3
Maria <mb.lev17@gmail.com>
2020-11-09 18:53:30 -0500

    Ordered, add words

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,7 @@ export class AppComponent {
 
   words = "";
   input = { "words": [] };
+  words_map: Map<String, String[]> = new Map<String, String[]>();
 
   constructor(private _service: ResultsService, private _router: Router,) { }
 
@@ -23,22 +24,36 @@ export class AppComponent {
     this.input.words = this.words.split(" ");
   }
 
+  getlist(key: String) {
+    return this.words_map.get(key);
+  }
+
   mapping(res: any) {
     let result: Map<String, number> = new Map<String, number>();
+
     for (let it in res) {
       res[it].forEach(element => {
         if (!result[element[0]]) {
           result.set(element[0], parseFloat(element[1]));
         }
+        if (this.words_map.has(element[0])) {
+          let array = this.words_map.get(element[0]);
+          array.push(it);
+          this.words_map.set(element[0], array);
+        } else {
+          this.words_map.set(element[0], [it]);
+        }
       });
     }
-    this.result = new Map([...result.entries()].sort((a, b) => b[1] - a[1]));
-    console.log(this.result);
+
+    this.result = new Array([...result.entries()].sort((a, b) => b[1] - a[1]));
+    //console.log(this.result);
+    //console.log(this.words_map);
   }
 
   getCoincidences() {
     this.split();
-    console.log(this.input);
+    //console.log(this.input);
     this._service.getAllTasks(this.input).subscribe(
       res => {
         this.mapping(res);
